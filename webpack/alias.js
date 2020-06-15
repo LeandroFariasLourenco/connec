1916e0f70b3eaec3dea4d78ca6f3202bad52663f
Leandro <leandro.farias01@outlook.com>
2020-06-15 00:20:39 -0300

    refactor(webpack): better setup for enviroment

---
+++ webpack/alias.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+  '@Components': path.resolve(__dirname, '../', 'src', 'js', 'components'),
+  '@Images': path.resolve(__dirname, '../', 'src', 'utils', 'images'),
+  '@Icons': path.resolve(__dirname, '../', 'src', 'utils', 'icons'),
+  '@Routes': path.resolve(__dirname, '../', 'src', 'js', 'routes')
+};
---
+++ webpack/rules.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = [
+  {
+    test: /\.js$/,
+    exclude: path.resolve(__dirname, '..', 'node_modules'),
+    use: ['babel-loader', 'eslint-loader']
+  },
+  {
+    test: /\.pug$/,
+    use: {
+      loader: 'pug-loader'
+    }
+  }
+];
